40c803d463556abfc977f43829c948302f9e3a7e
João Pedro Freitas de Oliveira <jpfreitas15@hotmail.com>
2024-08-05 09:32:57 -0300

    Prep for master

--- apps/galeria/models.py
+++ apps/galeria/models.py
@@ -15,6 +15,9 @@ class Veiculo(models.Model):
 
     def __str__(self) -> str:
         return self.nome.title()
+    
+    def show_preco(self) -> str:
+        return f'{self.preco:,.2f}'.replace(',', '.')
 class ImagemVeiculo(models.Model):
     veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE, related_name='imagens')
     imagem = models.ImageField(upload_to='attachments/%Y/%m', blank=True, max_length=255)
